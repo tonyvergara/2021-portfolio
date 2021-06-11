78d967d99e310b6fb0897c4fe40bd73388a7ae11
tonyvergara <antonio.vergara11@gmail.com>
2021-06-10 23:05:23 -0400

    Update forms.py

--- frontend/forms.py
+++ frontend/forms.py
@@ -3,4 +3,5 @@ from django import forms
 
 class Search_Form(forms.Form):
     search_term = forms.CharField(required=True)
-    
+    min_month = forms.IntegerField()
+    max_month = forms.IntegerField()
