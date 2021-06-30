e77b98c90531278d5c78dd76f0039096949527f5
tonyvergara <antonio.vergara11@gmail.com>
2021-06-29 22:25:35 -0400

    Update settings.py

--- django_contentful_demo/settings.py
+++ django_contentful_demo/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pe2804)*_b=^!%%hh@kat+tpv6lq!okdsmx$h&rexitu9ffc)p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
 
